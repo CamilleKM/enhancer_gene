ac2d3b33555b26b8488af35554e175055c5819d0
tfaraut <Thomas.Faraut@inra.fr>
2018-05-24 17:46:04 +0200

    modification de forme du script

--- alltoall_correlations.py
+++ alltoall_correlations.py
@@ -13,18 +13,22 @@ import warnings
 import scipy.stats as ss
 
 
+def eprint(*args, **kwargs):
+    ''' A simple to sys.stderr printer wrapper '''
+    print(*args, file=sys.stderr, **kwargs)
+
 
 class Dhs(object): #For each DHS.
     """
     Define each DHS peak as an object and compute firsts calculs: centered
     logcounts and standard deviation.
     """
-    def __init__(self,chrom,start,end,name,counts):
+    def __init__(self, chrom, start, end, name, counts):
         self._chrom = chrom
         self._start = int(start)
         self._end = int(end)
         self._name = name
-        self.counts = map(float,counts) #All counts are floats.
+        self.counts = counts
         self.sigma = np.std(self.log_counts)
         self.moy = np.mean(self.log_counts)
         self.log_counts_prime = np.array([i-self.moy for i in self.log_counts])
@@ -92,72 +96,58 @@ class Dhs(object): #For each DHS.
             return False
 
 
-def read_dhs_file(dhs_file, chromosome):
+def read_dhs_file(dhs_file, chromosome="all"):
     """
     Simply read the DHS file into an array of Dhs objectsfields
     """
     all_dhs = []  # Vector with all the object DHS from file_dnase.
     with open(dhs_file, "r") as f:
         for line in f:
+            if line.startswith("#"):
+                continue
             fields = line.rstrip().split("\t")
-            if fields[0] != "#chr":
-                if chromosome == "all":
-                    chrom, start, end, identifier = fields[0:4]
-                    start = int(start)
-                    end = int(end)
-                    counts = [float(c) for c in fields[4:]]
-                    all_dhs.append(Dhs(chrom, start, end, identifier, counts))
-                else:
-                    condition = "chr" + chromosome
-                    if fields[0] == condition:
-                        chrom, start, end, identifier = fields[0:4]
-                        start = int(start)
-                        end = int(end)
-                        counts = [float(c) for c in fields[4:]]
-                        all_dhs.append(Dhs(chrom, start, end, identifier, counts))
+            chrom, start, end, identifier = fields[0:4]
+            if chromosome != "all" and chrom != chromosome:
+                continue
+            start = int(start)
+            end = int(end)
+            counts = [float(c) for c in fields[4:]]
+            all_dhs.append(Dhs(chrom, start, end, identifier, counts))
+
     return all_dhs
 
 
-def write_correlations(output):
+def write_correlations(output, all_dhs):
 
     header = "\t".join(["#chr", "start", "end", "ID",
                         "chr", "start", "end", "ID",
-                        "Pearson_corr"])
-    if len(output[0]) == 3:
-        print(header)
-        for dhs1,dhs2,corr in output:
-            print("%s\t%s\t%5.4f" % (dhs1, dhs2, corr))
-    elif len(output[0]) == 4:
-        header_prime = "\t".join([header, "Spearman_corr"])
-        print(header_prime)
-        for dhs1,dhs2,corrP,corrS in output:
-            print("%s\t%s\t%5.4f\t%5.4f" % (dhs1, dhs2, corrP, corrS))
+                        "corr"])
+    print(header)
+    for i, j, corr in output:
+        dhs1 = all_dhs[i]
+        dhs2 = all_dhs[j]
+        print("%s\t%s\t%5.4f" % (dhs1, dhs2, corr))
 
 
-def dhs_arraytonumpy (all_dhs, corr):
+def arraytonumpy(all_dhs, method="pearson"):
     """
     Convert values from the class Dhs (logcounts and std) to numpy arrays for
     multiprocessing.
     Param: the table with all the object.
-    Return: two numpy array.
+    Return: 3 numpy arrays : logcounts, std, ranks
     """
-    table_logcounts = []
-    table_std = []
-    if corr == "p":
-        for i in all_dhs:
-            table_logcounts.append(i.log_counts_prime)
-            table_std.append(i.sigma)
-        return (np.array(table_logcounts), np.array(table_std))
-    else:
-        table_rangs = []
-        for i in all_dhs:
-            table_logcounts.append(i.log_counts_prime)
-            table_std.append(i.sigma)
-            table_rangs.append(i.rangs)
-        return (np.array(table_logcounts), np.array(table_std), \
-                np.array(table_rangs))
+    logcounts = []
+    stdeviation = []
+    ranks = []
+    for i in all_dhs:
+        logcounts.append(i.log_counts_prime)
+        stdeviation.append(i.sigma)
+        if method == "spearman":
+            ranks.append(i.rangs)
+    return (np.array(logcounts), np.array(stdeviation), np.array(ranks))
 
-def calcul_correlation (logcount1,logcount2,sigma1,sigma2, rangs, bis):
+
+def calcul_correlation(logcount1, logcount2, sigma1, sigma2, rangs, bis):
     """
     Compute the pearson correlation coefficient by hand thanks to calculs made
     in the class Dhs.
@@ -165,17 +155,40 @@ def calcul_correlation (logcount1,logcount2,sigma1,sigma2, rangs, bis):
     Return: pearson correlation coefficient.
     """
     if len(rangs) == 0:
-        numerator = np.dot(logcount1,logcount2)
+        numerator = np.dot(logcount1, logcount2)
         coefP = numerator/(10*sigma1*sigma2)
         return coefP
     else:
-        numerator = np.dot(logcount1,logcount2)
+        numerator = np.dot(logcount1, logcount2)
         coefP = numerator/(10*sigma1*sigma2)
         diff = 0
         for i in range(len(rangs)):
             diff += (rangs[i] - bis[i])**2
         coefS = 1 - (6*diff)/(10*((10**2)-1))
         return coefP, coefS
+
+
+def compute_correlation(logcounts, sigma, rank, i, j, mode="pearson"):
+        if mode == "pearson":
+            return pearsonr(logcounts[i], logcounts[j],
+                            sigma[i], sigma[j])
+        elif mode == "spearman":
+            return spearmanr(rank[i], rank[j])
+        else:
+            eprint("Unknown correlation method")
+            exit(1)
+
+
+def pearsonr(logcount_i, logcount_j, sigma_i, sigma_j):
+    numerator = np.dot(logcount_i, logcount_j)
+    coefP = numerator/(logcount_i.size*sigma_i*sigma_j)
+    return coefP
+
+
+def spearmanr(rank_i, rank_j):
+    n = len(rank_i)
+    d = np.sum([(rank_i[k] - rank_j[k])**2 for k in range(n)])
+    return 1-6*d/(n*(n**2-1))
 
 
 def parallel_correlations(args):
@@ -185,42 +198,41 @@ def parallel_correlations(args):
     Param: to_compute with the windows border index for each Dhs.
     Return: list of the correlation coefficient.
     """
-    start,end = args
+    start, end, mode = args
 
-    with warnings.catch_warnings(): #Warning due to the use of global variables.
+    # Warning is necessary because of the use of global variables
+    # in parallelization mode
+    with warnings.catch_warnings():
         warnings.simplefilter('ignore', RuntimeWarning)
         logcounts = np.ctypeslib.as_array(logcounts_global)
         sigma = np.ctypeslib.as_array(sigma_global)
-        rangs = np.ctypeslib.as_array(rangs_global)
+        ranks = np.ctypeslib.as_array(ranks_global)
 
     correlations = []
+    i = start
     for j in range(start+1, end+1):
-        if len(rangs) == 0 :
-            r = calcul_correlation(logcounts[start], logcounts[j], \
-                                    sigma[start], sigma[j], rangs, rangs)
-            correlations.append(((start, j), r))
-        else:
-            (r, rs) = calcul_correlation(logcounts[start], logcounts[j], \
-                                    sigma[start], sigma[j], \
-                                    rangs[start], rangs[j])
-            correlations.append(((start, j), r, rs))
+        r = compute_correlation(logcounts, sigma, ranks,  i, j, mode)
+        correlations.append(((i, j), r))
+
     return correlations
 
 
-def _init_parallel(logcounts_to_populate, sigma_to_populate, rangs_to_populate):
+def _init_parallel(logcounts_to_populate, sigma_to_populate,
+                   ranks_to_populate):
     """
     Each pool process calls this initializer.
     Load the arrays to be populated into that process's global namespace
     """
     global logcounts_global
     global sigma_global
-    global rangs_global
+    global ranks_global
     logcounts_global = logcounts_to_populate
     sigma_global = sigma_to_populate
-    rangs_global = rangs_to_populate
+    ranks_global = ranks_to_populate
 
 
-def parallel_implementation_corr(logcounts, sigma, to_compute, rangs=[], num_proc=1):
+def parallel_implementation_corr(logcounts, sigma, ranks, to_compute,
+                                 threads=1):
     """
     Create shared variables and pool for multiprocessing.
     Params: logcounts and sigma numpy array, to_compute list with the index and
@@ -228,22 +240,25 @@ def parallel_implementation_corr(logcounts, sigma, to_compute, rangs=[], num_pro
     Return: all the correlations and index
     """
     # Define the corresponding shared ctype arrays
-    tmp_logcounts = np.ctypeslib.as_ctypes(logcounts) #Create ctypes object from numpy array
-    shared_logcounts = sharedctypes.Array(tmp_logcounts._type_, tmp_logcounts, lock=False)
-    #                       Return a ctypes array allocated from shared memory.
-    #                       tmp_dhs._type_: type of the returned array elements
-    #                       tmp_dhs: initialize the array
-    #                       lock=True (default): synchronize access to the value
-    #                       lock=False: access to returned object not protected
+    # Create ctypes object from numpy array
+    tmp_logcounts = np.ctypeslib.as_ctypes(logcounts)
+    shared_logcounts = sharedctypes.Array(tmp_logcounts._type_, tmp_logcounts,
+                                          lock=False)
+    #     Returns a ctypes array allocated from shared memory.
+    #     tmp_dhs._type_: type of the returned array elements
+    #     tmp_dhs: initialize the array
+    #     lock=True (default): synchronize access to the value
+    #     lock=False: access to returned object not protected
 
     tmp_sigma = np.ctypeslib.as_ctypes(sigma)
-    shared_sigma = sharedctypes.Array(tmp_sigma._type_,tmp_sigma, lock=False)
+    shared_sigma = sharedctypes.Array(tmp_sigma._type_, tmp_sigma, lock=False)
 
-    tmp_rangs = np.ctypeslib.as_ctypes(rangs)
-    shared_rangs = sharedctypes.Array(tmp_rangs._type_,tmp_rangs, lock=False)
+    tmp_ranks = np.ctypeslib.as_ctypes(ranks)
+    shared_ranks = sharedctypes.Array(tmp_ranks._type_, tmp_ranks,
+                                          lock=False)
 
-    pool = Pool(processes=num_proc, initializer=_init_parallel,
-                initargs=(shared_logcounts, shared_sigma, shared_rangs, ))
+    pool = Pool(processes=threads, initializer=_init_parallel,
+                initargs=(shared_logcounts, shared_sigma, shared_ranks, ))
     #                       controls a pool of worker processes
     #                       processes: number of worker processes to use
     #                       initializer is not None: each worker process will
@@ -256,7 +271,7 @@ def parallel_implementation_corr(logcounts, sigma, to_compute, rangs=[], num_pro
     return results
 
 
-def sort_correlations(correlations,all_dhs):
+def sort_correlations(correlations, all_dhs):
     """
     Sort the result from multiprocessing and add the data corresponding to each
     coefficient.
@@ -264,56 +279,77 @@ def sort_correlations(correlations,all_dhs):
     object Dhs
     Result: sorted numpy array with the results to print.
     """
-    if len(correlations) == 3:
-        sorted_cor = [[all_dhs[cell[0][0]],all_dhs[cell[0][1]],cell[1]] \
-                    for cell in sorted(correlations, \
-                                       key=lambda x:(x[0][0], x[0][1]))]
-    else:
-        sorted_cor = [[all_dhs[cell[0][0]],all_dhs[cell[0][1]],cell[1],cell[2]] \
-                    for cell in sorted(correlations, \
-                                       key=lambda x:(x[0][0], x[0][1]))]
-    return np.array(sorted_cor)
+    results = []
+    for cor in sorted(correlations,  key=lambda x: (x[0][0], x[0][1])):
+        pair, r = cor
+        i, j = pair
+        results.append((i, j, r))
+    return results
 
 
-if __name__ == '__main__':
-    #Input from command line.
-    parser = ap.ArgumentParser()
-    parser.add_argument("file_dnase", help="file with the read count per peak")
-    parser.add_argument("-c", "--chrom", help="if you want the calcul only on \
-                        one chromosome.")
-    parser.add_argument("-d", "--distance", help="distance maximum between peaks\
-                        to calcul the correlation. (default:500000)",
-                        default=500000,type=int)
-    parser.add_argument("-s", "--spearman", help="to calcul also the Spearman \
-                        correlation.", action="store_true")
-    args = parser.parse_args()
-
-    if args.chrom: #We can choose to calcul correlation in one chromosome only.
-        all_dhs = read_dhs_file(args.file_dnase, args.chrom)
-    else:
-        all_dhs = read_dhs_file(args.file_dnase,"all")
-
+def get_pairs_tocompare(all_dhs, distance, method="pearson"):
+    """
+    Identify the paris verifying the distance constraints hence the pairs
+    for whixh a correlation will be computed
+    """
     to_compute = []
     for i in range(len(all_dhs)-1):
         right_border = i+1
-        while all_dhs[i].within_neighbourhood(\
-                                              all_dhs[right_border].interval,\
-                                              args.distance):
+        while all_dhs[i].within_neighbourhood(all_dhs[right_border].interval,
+                                              distance):
             right_border += 1
             if right_border == len(all_dhs):
                 break
-        to_compute.append([i,right_border-1]) #For each peak, we compute
-        #correlation with all the peaks in this window.
+        # For each peak, we compute correlation with all
+        # the peaks in this window.
+        to_compute.append([i, right_border-1, method])
+    return to_compute
 
-    num_proc = 8
-    if args.spearman:
-        (logcounts,sigma,rangs) = dhs_arraytonumpy(all_dhs,"s")
-        output = parallel_implementation_corr(logcounts, sigma, to_compute, \
-                                              rangs, num_proc)
-    else:
-        (logcounts,sigma) = dhs_arraytonumpy(all_dhs,"p")
-        output = parallel_implementation_corr(logcounts, sigma, to_compute, \
-                                              num_proc)
-    sorted_correlations = sort_correlations(output,all_dhs)
 
-    write_correlations(sorted_correlations)
+def compute_all_correlations(all_dhs, distance=500000,
+                             method="pearson", threads=1):
+    to_compute = get_pairs_tocompare(all_dhs, distance, method)
+    (logcounts, sigma, ranks) = arraytonumpy(all_dhs, method=method)
+    output = parallel_implementation_corr(logcounts, sigma, ranks,
+                                          to_compute, threads=threads)
+    return output
+
+
+if __name__ == '__main__':
+    # Input from command line.
+    parser = ap.ArgumentParser()
+    parser.add_argument("file_dnase", help="file with the read count per peak")
+    parser.add_argument("-c", "--chrom", help="if you want the calcul only on \
+                        one chromosome.", default="all", type=str)
+    parser.add_argument("-d", "--distance", help="distance maximum between peaks\
+                        to calcul the correlation. (default:500000)",
+                        default=500000, type=int)
+    parser.add_argument("-m", "--method", help="correlation ncoefficient, \
+                        pearson or spearman. (default:pearson)",
+                        default="pearson", type=str)
+    parser.add_argument("-t", "--threads", help="number of prcessors\
+                         (default:1)",
+                        default=1, type=int)
+    args = parser.parse_args()
+
+    dnase_file = args.file_dnase
+    distance = args.distance
+    threads = args.threads
+    chrom = args.chrom
+    method = args.method
+
+    eprint("Reading dhs file")
+    all_dhs = read_dhs_file(dnase_file, chrom)
+    eprint("%d peaks loaded" % (len(all_dhs)))
+    eprint("Identifying correlation pairs to compute")
+    to_compute = get_pairs_tocompare(all_dhs, distance)
+
+    eprint("Computing the correlations")
+    output = compute_all_correlations(all_dhs, distance=distance,
+                                      method=method, threads=threads)
+
+    eprint("Sorting the correlations")
+    sorted_correlations = sort_correlations(output, all_dhs)
+
+    eprint("Writing the correlations")
+    write_correlations(sorted_correlations, all_dhs)
